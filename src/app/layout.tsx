2929a0513164c1bda6953534388a33d8faf5b76a
洋 <94534613+BoyYangzai@users.noreply.github.com>
2024-04-09 22:30:44 +0800

    feat: add antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -11,11 +12,13 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <AntdRegistry>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
             {props.children}
-          </ThemeProvider>
+            </ThemeProvider>
+            </AntdRegistry>
         </AppRouterCacheProvider>
       </body>
     </html>
